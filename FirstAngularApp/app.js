72e1e333d3cdab8fba818b1857042043205313af
bhavyagupta98 <bhavyagupta@live.com>
2020-05-31 14:40:32 +0530

    Test Application

--- FirstAngularApp/app.js
+++ FirstAngularApp/app.js
@@ -3,11 +3,21 @@
 
 angular.module('myFirstApp', [])
 
-.controller('MyFirstController', function ($scope) {
-  $scope.name = "Yaakov";
-  $scope.sayHello = function () {
-    return "Hello Coursera!";
+.controller('MyNameCalculator', function ($scope) {
+  $scope.name = "";
+  $scope.val=0;
+  $scope.nameValueCalculator = function () {
+    var temp = func($scope.name);
+    $scope.val = temp;
   };
+
+  function func(string){
+    var value = 0;
+    for (var i=0;i<string.length;i++){
+      value += string.charCodeAt(i);
+    }
+    return value;
+  }
 });
 
 })();
